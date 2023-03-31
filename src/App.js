9b689a1e546298e8ba75a6cc566580a5fca502c5
CptGrave <lukastrevo@gmail.com>
2023-03-31 09:33:58 +0200

    setting dicearray to obj

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import Die from './components/Die'
 function App() {
+  
+  const [diceArray, setDiceArray] = React.useState(allNewDice())
+  
+  function allNewDice() {
+    const newDice = []
+    for (let i = 0; i < 10; i++) {
+        newDice.push(Math.ceil(Math.random() * 6))
+    }
+    return newDice
+}
+  const dieElements = diceArray.map(elem => <Die value={elem}/>)
+  
+  function rollDice() {
+    setDiceArray(allNewDice)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="container">
+        {dieElements}
+      </div>
+      <button className="roll-button" onClick={rollDice}>Roll</button>
+    </main>
+    
   );
 }
 
