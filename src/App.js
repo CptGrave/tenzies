80aea95ca8380ee020b9bbd03e50f4e6f4d60a1d
CptGrave <lukastrevo@gmail.com>
2023-04-03 23:39:05 +0200

    add rollDice update and title

--- src/App.js
+++ src/App.js
@@ -5,18 +5,16 @@ import { nanoid } from 'nanoid';
 
 function App() {
 
-  /**
- * Challenge: Update the `holdDice` function to flip
- * the `isHeld` property on the object in the array
- * that was clicked, based on the `id` prop passed
- * into the function.
+/**
+ * Challenge: Update the `rollDice` function to not just roll
+ * all new dice, but instead to look through the existing dice
+ * to NOT role any that are being `held`.
  * 
- * Hint: as usual, there's > 1 way to accomplish this.
- * I'll be using `dice.map()` and checking for the `id`
- * of the die to determine which one to flip `isHeld` on,
- * but you can do whichever way makes the most sense to you.
+ * Hint: this will look relatively similiar to the `holdDice`
+ * function below. When creating new dice, remember to use
+ * `id: nanoid()` so any new dice have an `id` as well.
  */
-  
+
   const [diceArray, setDiceArray] = React.useState(allNewDice())
   
   function allNewDice() {
@@ -34,17 +32,24 @@ function App() {
   const dieElements = diceArray.map(elem => <Die key={elem.id} value={elem.value} isHeld={elem.isHeld} holdDice={()=>{holdDice(elem.id)}}/>)
   
   function rollDice() {
-    setDiceArray(allNewDice)
+    setDiceArray(oldArray => oldArray.map(die => {
+      return die.isHeld === true ? {...die} : { 
+        value: Math.ceil(Math.random() * 6),
+        isHeld:false,
+        id:nanoid()}
+    }))
   }
 
   function holdDice(id) {
-    setDiceArray(oldDice => oldDice.map(die => {
-      return die.id === id ? {...die, isHeld: !die.isHeld} : {...die}
+    setDiceArray(oldArray => oldArray.map(die => {
+      return die.id === id ? {...die, isHeld:!die.isHeld} : {...die}
     }))
   }
 
   return (
     <main>
+      <h1 className="title">Tenzies</h1>
+        <p className="instructions">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
       <div className="container">
         {dieElements}
       </div>
