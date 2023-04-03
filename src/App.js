d7ec97cee7ab48f806d1492e169f73a333f9822d
CptGrave <lukastrevo@gmail.com>
2023-04-03 23:10:38 +0200

    add holdDice function

--- src/App.js
+++ src/App.js
@@ -4,6 +4,18 @@ import Die from './components/Die'
 import { nanoid } from 'nanoid';
 
 function App() {
+
+  /**
+ * Challenge: Update the `holdDice` function to flip
+ * the `isHeld` property on the object in the array
+ * that was clicked, based on the `id` prop passed
+ * into the function.
+ * 
+ * Hint: as usual, there's > 1 way to accomplish this.
+ * I'll be using `dice.map()` and checking for the `id`
+ * of the die to determine which one to flip `isHeld` on,
+ * but you can do whichever way makes the most sense to you.
+ */
   
   const [diceArray, setDiceArray] = React.useState(allNewDice())
   
@@ -19,10 +31,16 @@ function App() {
     return newDice
 }
 
-  const dieElements = diceArray.map(elem => <Die key={elem.id} value={elem.value} isHeld={elem.isHeld}/>)
+  const dieElements = diceArray.map(elem => <Die key={elem.id} value={elem.value} isHeld={elem.isHeld} holdDice={()=>{holdDice(elem.id)}}/>)
   
   function rollDice() {
     setDiceArray(allNewDice)
+  }
+
+  function holdDice(id) {
+    setDiceArray(oldDice => oldDice.map(die => {
+      return die.id === id ? {...die, isHeld: !die.isHeld} : {...die}
+    }))
   }
 
   return (
